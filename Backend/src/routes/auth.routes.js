cb1ed1e3badcad9a9b8c4e000689dd9889b800ac
Parshva <codewithpasshu@gmail.com>
2026-02-21 09:57:51 +0530

    Backend Is ready

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authcontroller = require("../controller/auth.controller");
+const identifyUser = require('../middlewares/auth.middleware')
 
 const authRouter = express.Router();
 
 authRouter.post("/register", authcontroller.registerController);
 authRouter.post("/login", authcontroller.loginController);
+authRouter.get('/get-me',identifyUser,authcontroller.getMeController)
 
 module.exports = authRouter;
